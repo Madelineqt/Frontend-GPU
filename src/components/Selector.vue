fb39c65fb8afb24cd695406373fc81c72fdd0058
uwu <afbritezr41@alumnes.santjosepobrer.es>
2021-01-25 00:12:03 +0100

    ad

--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,7 +1,19 @@
 <template>
 <div>
+ <section id="intro" class="slide-in-top">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <img src="logoSJO.png">
+                <h1 class="display-4" style="font-size: 2.5rem;">Ver reservas en tabla</h1>
+            </div>
+        </div>
+    </section>
 
-  <div class="row">
+    <section id="booking" class="slide-in-bottom">
+      <div id="danger-alerts" v-if="error">
+        <div class="alert alert-danger" role="alert"> {{alert}}</div>
+      </div>
+        <div class="row">
     <div class="col-6 ">
       <div class="input-group mb-3">
         <div class="input-group-prepend">
@@ -33,6 +45,7 @@
     </div>
   </div>
    <div class="row">
+      
       <div class="col-6 ">
         <div class="imggpu"><img v-bind:src="img1"></div>
       </div>
@@ -363,6 +376,100 @@
       </div>
    </div>
    </div>
+    </section>
+    <section id="footer" class="slide-in-bottom">
+        <div class="container">
+            <footer class="page-footer font-small blue pt-4">
+
+                <!-- Footer Links -->
+                <div class="container-fluid text-center text-md-left">
+
+                    <!-- Grid row -->
+                    <div class="row white" style="border: initial !important">
+
+                        <!-- Grid column -->
+                        <div class="col-md-4 mt-md-0 mt-4">
+                            <h5 class="text-uppercase">SANT JOSEP OBRER</h5>
+                            <!-- Content -->
+                            <p>
+                                <br>
+                                La educación en la escuela es todo un RETO en el siglo XXI. Y queremos vivir este reto con mucha ilusión, proponiéndonos metas altas, para llegar a lo más alto posible!!!
+                                <br>
+                                No somos perfectos, pero no tenemos miedo de equivocarnos, de aprender de los errores, corregirlos y MEJORAR, porque lo más importante es CREER en lo que haces, y éste es el verdadero aprendizaje!!!</p>
+
+                        </div>
+                        <!-- Grid column -->
+
+                        <hr class="clearfix w-100 d-md-none pb-3">
+                        <div class="col-md-4 mt-md-0 mt-4">
+
+                            <!-- Content -->
+                            <h5 class="text-uppercase">COMPARTE SJO</h5>
+
+                            <button type="button"
+                              
+                                class="btn btn-fb"><i style="color:white;" class="fab fa-facebook-f"></i></button>
+                            <button type="button"
+                              
+                                class="btn btn-fb"><i style="color:white;" class="fab fa-google-plus-g"></i></button>
+                            <button type="button"
+                              
+                                class="btn btn-fb"><i style="color:white;" class="fab fa-twitter"></i></button>
+                            <button type="button"
+                            
+                                class="btn btn-fb"><i style="color:white;" class="fab fa-linkedin-in"></i></button>
+                        </div>
+                        <!-- Grid column -->
+                        <hr class="clearfix w-100 d-md-none pb-3">
+                        <div class="col-md-4 mb-md-0 mb-4">
+
+                            <!-- Links -->
+                            <h5 class="text-uppercase">Enlaces</h5>
+
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a style="color:white;"
+                                        href="#">Política de
+                                        privacidad</a>
+                                </li>
+                                <li>
+                                    <a style="color:white;"
+                                        href="#">Política
+                                        de calidad</a>
+                                </li>
+                                <li>
+                                    <a style="color:white;" href="#">Política de
+                                        Cookies</a>
+                                </li>
+                                <li>
+                                    <a style="color:white;" href="#">Aviso
+                                        legal</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                        <!-- Grid column -->
+
+
+
+                    </div>
+                    <!-- Grid row -->
+
+                </div>
+                <!-- Footer Links -->
+
+                <!-- Copyright -->
+                <div class="footer-copyright text-center py-3">© 2020 :
+                    <a style="color:white;" href="#"> SJO</a>
+                </div>
+                <!-- Copyright -->
+
+            </footer>
+        </div>
+
+        <!-- Footer -->
+    </section>
+ 
    
    
 </div>
@@ -375,6 +482,8 @@ import axios from 'axios'
 export default {
   name: 'Selector',
   data: () => ({
+    alert: "",
+    error: false,
     gpus: [],
     gpu1: "",
     gpu2: "",
@@ -409,6 +518,14 @@ export default {
     compare: function(){
       this.gpu1 = this.gpus[this.gpus.findIndex(i => i._id === this.idgpu1)]
       this.gpu2 = this.gpus[this.gpus.findIndex(i => i._id === this.idgpu2)]
+      console.log(this.gpu1, this.gpu2)
+      if (this.gpu1 === undefined || this.gpu2 === undefined){
+        this.alert = "Una de las opciones es inválida"
+        this.error = true
+        
+        return
+      }
+      this.error = false
       this.resultsappear = true
 
     }
@@ -416,6 +533,135 @@ export default {
 }
 </script>
 <style scoped>
+
+
+ @-webkit-keyframes slide-in-bottom{0%{-webkit-transform:translateY(1000px);transform:translateY(1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes slide-in-bottom{0%{-webkit-transform:translateY(1000px);transform:translateY(1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}
+ @-webkit-keyframes slide-in-top{0%{-webkit-transform:translateY(-1000px);transform:translateY(-1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}@keyframes slide-in-top{0%{-webkit-transform:translateY(-1000px);transform:translateY(-1000px);opacity:0}100%{-webkit-transform:translateY(0);transform:translateY(0);opacity:1}}
+
+
+ .slide-in-bottom{-webkit-animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) both;animation:slide-in-bottom .5s cubic-bezier(.25,.46,.45,.94) both}
+ .slide-in-top{-webkit-animation:slide-in-top .5s cubic-bezier(.25,.46,.45,.94) both;animation:slide-in-top .5s cubic-bezier(.25,.46,.45,.94) both}
+ .jumbotron img {
+    width: 400px;
+}
+.white {
+    color: white !important;
+}
+.jumbotron{
+    text-align: center;
+    padding: 5%;
+    background-image: url('/bg.png');
+    background-size: cover;
+    color: white;
+}
+.jumbotron h1{
+    margin-top: 20px;
+}
+.hide {
+    display: none;
+}
+.form-control{
+    border-radius: 1px;
+    background-color: #ededed;
+    border:none;
+}
+.minute-controller{
+    display: flex;
+}
+small a {
+    color:white;
+}
+#footer{
+    
+    background-color: #BA172C;
+    color: white;
+    margin-top: 100px;
+
+}
+@media only screen and (min-width: 1370px) {
+#booking{
+    background-color: white;
+    border-radius: 6px;
+    box-shadow: 0 2px 43px -4px rgba(0,0,0,.19);
+    padding:3%;
+    width: auto;
+    position: relative;
+    margin-left: 20%;
+    margin-right: 20%;
+    top: 35%;
+}
+body, html { overflow-x:hidden !important }
+}
+@media only screen and (max-width: 1370px) {
+
+    #booking{
+       background-color: white;
+        border-radius: 6px;
+        box-shadow: 0 2px 43px -4px rgba(0,0,0,.19);
+        padding: 3%;
+        width: auto;
+        position: relative;
+        margin-left: 2%;
+        margin-right: 2%;
+    }
+}
+@media only screen and (max-width: 600px) {
+    .descriptions{
+        position: absolute;
+        top: 270%;
+        left: 2%;
+        right: 2%;
+    }
+}
+.btn-primary {
+    background-color: #BA172C;
+    border-color: #BA172C;
+    
+}
+
+.btn-primary:hover{
+    background-color: #BA172C;
+    border-color: #BA172C; 
+}
+.btn-primary:focus{
+    background-color: #BA172C !important;
+    border-color: #BA172C !important;
+}
+.btn-primary:active{
+    background-color: #BA172C;
+    border-color: #BA172C;
+}
+.btn-outline-primary{
+    border-color: #BA172C;
+    color: #BA172C;
+}
+.btn-outline-primary:hover{
+    background-color:#BA172C;
+}
+.btn-hours{
+    margin: 2%;
+}
+.btn-primary.disabled, .btn-primary:disabled {
+    color: #fff;
+    background-color:  #BA172C;
+    border-color:  #BA172C;
+}
+.btn-outline-primary.disabled, .btn-outline-primary:disabled {
+    color:  #BA172C;
+    background-color: transparent;
+}
+.row.white{
+    text-align: center !important;
+}
+@media only screen and (max-width: 600px) {
+
+    .jumbotron img {
+        width: 100%;
+    }
+}
+html, body {
+    overflow-x: hidden;
+  }
 .imggpu, .progressbar{
   width: 50%;
   height: 300px;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import VueMeta from 'vue-meta';
-
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 
 Vue.config.productionTip = false
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.use(BootstrapVue)
 Vue.use(VueMeta);
 new Vue({
